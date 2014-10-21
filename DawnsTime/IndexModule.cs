7a1b580848682db7957ba70521f617349310d5b2
Shahein <shahein.moussavi@izenda.com>
2014-10-20 20:07:50 -0400

    First commit

---
+++ DawnsTime/IndexModule.cs
@@ -0,0 +1,15 @@
+﻿using Nancy;
+
+namespace DawnsTime
+{
+    public class TimeModule : NancyModule
+    {
+        public TimeModule()
+        {
+            Get["/"] = parameters =>
+            {
+                return View["index"];
+            };
+        }
+    }
+}
